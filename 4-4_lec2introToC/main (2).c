cf916c809b17ba616ec78c56873b0485216e5e17
Sharma Natvar <121554685+SharmaNatvar@users.noreply.github.com>
2023-04-19 11:04:09 +0530

    Add files via upload

---
+++ 4-4_lec2introToC/main (2).c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf(" -\t-\t- \n");
+    printf("|\t \t |\n");
+    printf("N\t \t |\n");
+    printf("A\t \t |\n");
+    printf("M\t \t |\n");
+    printf("E\t \t |\n");
+    printf("|\t \t |\n");
+    printf(" -\t-\t- ");
+
+    return 0;
+}
