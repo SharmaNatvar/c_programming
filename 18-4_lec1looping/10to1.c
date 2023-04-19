98f24356031b5b0951b22f3803581c62d9de3abd
Sharma Natvar <121554685+SharmaNatvar@users.noreply.github.com>
2023-04-19 11:26:11 +0530

    Add files via upload

---
+++ 18-4_lec1looping/10to1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	
+//	int a = 10;
+	
+	int a;
+	
+	printf("enter your value : ");
+	scanf("%d",&a);
+	
+	while(a>=1){
+		
+		printf("%d \n",a);
+		
+		a--;
+	}
+	
+	return 0;
+}
---
+++ 18-4_lec1looping/1to10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a =1;
+	
+	
+	
+//	int a;
+//	printf("enter your first number : ");
+//	scanf("%d",&a);
+	
+	
+	while(a<=10){
+		printf("%d \n",a);		
+		a++;
+	}
+	
+	return 0;
+} 
