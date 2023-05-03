09c7303d42295be1af390bd27426b8af37563b59
Sharma Natvar <121554685+SharmaNatvar@users.noreply.github.com>
2023-05-03 07:14:46 +0530

    Add files via upload

---
+++ 26-4_Circlet/21ques.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	
+	int r,c,num=1;
+	
+	
+	for(r=1; r<=5 ; r++){
+		for(c=1; c<=r; c++){
+			printf("%d ",num);
+			num++;
+		}
+		printf("\n");
+	}
+	
+	
+	
+	return 0;
+}
+
+
