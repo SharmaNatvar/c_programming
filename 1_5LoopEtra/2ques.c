4850926f371583594c234f247ad55f2c129a83c8
Sharma Natvar <121554685+SharmaNatvar@users.noreply.github.com>
2023-05-08 06:57:30 +0530

    Add files via upload

---
+++ 1_5LoopEtra/2ques.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+
+main() {
+  int n, i, j = 0;
+  
+  
+  printf("Enter any number n:");
+  scanf("%d", &n);
+  
+
+  for (i = 1; i <= n; i++) {
+	  if (n % i == 0) {
+	     j++;
+	  }
+  }
+
+  if (j == 2) {
+        printf("n is a Prime number");
+  }
+  else {
+         printf("n is not a Prime number");
+  }
+
+
+
+  return 0;    
+}
