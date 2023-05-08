39f0056ae829bcb225a0cfc9d1c243ba1f4485f0
Sharma Natvar <121554685+SharmaNatvar@users.noreply.github.com>
2023-05-08 10:27:34 +0530

    Update 5ques.c

--- 3-5_Assortment/5ques.c
+++ 3-5_Assortment/5ques.c
@@ -6,24 +6,23 @@ int main() {
     int r, c;
 
     printf("Enter the number of rows : ");
-    scanf("%d ", &r);
+    scanf("%d ",&r);
+    
     printf("Enter the number of columns : ");
     scanf("%d",&c);
 
-    int a[r][c];
-    int i ,j,sum;
+    int a[r][c],i ,j,sum=0;
 
-
+        	printf("Enter the elements : ");
     for (i = 0; i < r; i++) {
         for (j = 0; j < c; j++) {
-        	printf("Enter the elements : ");
+
             scanf("%d", &a[i][j]);
         }
     }
 
     printf("The colm sum : ");
     for (i = 0; i < c; i++) {
-        sum = 0;
         for (j = 0; j < c; j++) {
             sum += a[i][j];
         }
